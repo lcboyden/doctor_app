57f0aa1934154eea60528a37c5b3257ef811bfa2
Caden Biddinger <caden@Cadens-MacBook-Air.local>
2020-05-21 16:03:17 -0600

    got seeds set up

--- Gemfile
+++ Gemfile
@@ -30,7 +30,9 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'faker'
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem "pry-rails"
 end
 
 group :development do
@@ -40,7 +42,6 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
-  gem "pry-rails"
 end
 
 group :test do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# roles = ['teacher', 'ta', 'student']
+# 10.times do
+#   course = Course.create(
+#     name: Faker::Educator.course_name
+#   )
+ 
+#   10.times do 
+#     user = User.create(
+#       first_name: Faker::Name.first_name,
+#       last_name: Faker::Name.last_name
+#     )
+
+#     Enrollment.create(
+#       role: roles.sample,
+#       course_id: course.id,
+#       user_id: user.id
+#     )
+#   end
+# end
+
+
+
+5.times do 
+  @doctor = Doctor.create(
+    first_name: "Dr.",
+    last_name: Faker::Movies::StarWars.character
+    )
+
+  5.times do 
+  @user = User.create(
+    first_name: Faker::Movies::LordOfTheRings.character,
+    last_name: Faker::Movies::LordOfTheRings.character
+    )
+    
+    @appts = Appointment.create(
+      date: Faker::Date.between(from: Date.today, to: 2.days.from_now),
+      time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :short),
+      user_id: @user.id,
+      doctor_id: @doctor.id,
+      )
+    end
+  end
+  
+  puts "Data Seeded"
